3c0d8fe488a27efe528cd1bf9a094009a3d4ca16
Leo Calogero <leo.calogero@gmail.com>
2019-01-08 02:25:53 -0500

    adding konami code

--- js/custome.js
+++ js/custome.js
@@ -65,6 +65,11 @@ window.onload = function() {
 
 var swiper = new Swiper('.product-slider', {
     spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
     effect: 'fade',
     // initialSlide: 2,
     loop: true,
@@ -81,7 +86,6 @@ var swiper = new Swiper('.product-slider', {
 
             var target = $('.product-slider__item').eq(index).data('target');
 
-            console.log(target);
 
             $('.product-img__item').removeClass('active');
             $('.product-img__item#'+ target).addClass('active');
@@ -95,7 +99,6 @@ swiper.on('slideChange', function () {
 
     var target = $('.product-slider__item').eq(index).data('target');
 
-    console.log(target);
 
     $('.product-img__item').removeClass('active');
     $('.product-img__item#'+ target).addClass('active');
@@ -117,12 +120,17 @@ swiper.on('slideChange', function () {
 
 var swiper = new Swiper('.blog-slider', {
     spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination-2',
+        type: 'bullets',
+        clickable: true,
+    },
     effect: 'fade',
     // initialSlide: 2,
     loop: true,
     navigation: {
         nextEl: '.next',
-        prevEl: '.prev'
+        prevEl: '.prev',
     },
     // mousewheel: {
     //     // invert: false
@@ -132,8 +140,6 @@ var swiper = new Swiper('.blog-slider', {
             var index = this.activeIndex;
 
             var target = $('.blog-slider__item').eq(index).data('target');
-
-            console.log(target);
 
             $('.blog-img__item').removeClass('active');
             $('.blog-img__item#'+ target).addClass('active');
@@ -146,8 +152,6 @@ swiper.on('slideChange', function () {
     var index = this.activeIndex;
 
     var target = $('.blog-slider__item').eq(index).data('target');
-
-    console.log(target);
 
     $('.blog-img__item').removeClass('active');
     $('.blog-img__item#'+ target).addClass('active');
@@ -191,3 +195,47 @@ $('.scroll').on('click',function(e) {
 		// window.location.hash = target;
 	});
 })
+
+// a key map of allowed keys
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+// the 'official' Konami Code sequence
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+// a variable to remember the 'position' the user has reached so far.
+var konamiCodePosition = 0;
+
+// add keydown event listener
+document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+  if (key == requiredKey) {
+
+    // move to the next key in the konami code sequence
+    konamiCodePosition++;
+
+    // if the last key is reached, activate cheats
+    if (konamiCodePosition == konamiCode.length) {
+      activateCheats();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+});
+
+function activateCheats() {
+  console.log('Konami!')
+}
+
